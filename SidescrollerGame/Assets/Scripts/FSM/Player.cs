eb41380b6667e874d71c1aef6ec908146f91e317
Frederic <flevesque124@gmail.com>
2023-07-15 21:54:58 -0400

    Add jump and ground check

--- SidescrollerGame/Assets/Scripts/FSM/Player.cs
+++ SidescrollerGame/Assets/Scripts/FSM/Player.cs
@@ -10,6 +10,18 @@ public class Player : MonoBehaviour
     public float moveSpeed = 12f;
     public float jumpForce;
 
+    [Header("Collision Check")]
+    [SerializeField] private LayerMask collisionMask; 
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float wallCheckDistance;
+    [SerializeField] private Transform wallCheck; 
+    [SerializeField] private float ceillingCheckDistance;
+    [SerializeField] private Transform ceillingCheck;
+    
+    private bool isWallDetected;
+    private bool ceillingDetected;
+
 #region Components
     public Animator    anim { get; private set;}
     public Rigidbody2D rb { get; private set;}
@@ -43,9 +55,57 @@ public class Player : MonoBehaviour
 
     private void Update() {
         stateMachine.currentState.Update();
+
+        
     }
 
     public void SetVelocity(float _xVelocity, float _yVelocity){
         	rb.velocity = new Vector2(_xVelocity, _yVelocity);
+    }
+
+    public bool IsGroundDetected() => Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, collisionMask);
+
+    public bool IsWallDetected() => Physics2D.Raycast(wallCheck.position, Vector2.right, wallCheckDistance, collisionMask);
+
+    public void CollisionCheck(){
+        isWallDetected = Physics2D.Raycast(wallCheck.position, Vector2.right, wallCheckDistance, collisionMask);
+        ceillingDetected = Physics2D.Raycast(ceillingCheck.position,Vector2.up,ceillingCheckDistance, collisionMask);
+    }
+
+
+    protected virtual void OnDrawGizmos() {        
+
+        if(ceillingDetected)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(ceillingCheck.position, new Vector3(ceillingCheck.position.x, ceillingCheck.position.y - ceillingCheckDistance));  
+        }
+        else
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(ceillingCheck.position, new Vector3(ceillingCheck.position.x, ceillingCheck.position.y - ceillingCheckDistance));          
+        }
+
+        if(IsGroundDetected())
+        {  
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
+        }
+        else
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));          
+        }
+
+        if(IsWallDetected())
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));    
+        }
+        else
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));          
+        }
     }
 }
