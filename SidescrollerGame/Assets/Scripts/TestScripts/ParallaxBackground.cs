197c6d726a769beae7ee703edbd1508fdb244598
Frederic <flevesque124@gmail.com>
2023-07-31 20:34:50 -0400

    Parallax background added
    Parallax background will move with the player movement

---
+++ SidescrollerGame/Assets/Scripts/TestScripts/ParallaxBackground.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+
+    private GameObject camera;
+
+    [SerializeField] private float parallaxEffect;
+
+    private float xPosition;
+    private float spriteLength;
+
+    void Start()
+    {
+        camera = GameObject.Find("Main Camera");
+        xPosition = transform.position.x;
+        spriteLength = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void Update()
+    {
+        float distanceMoved =  camera.transform.position.x * (1 - parallaxEffect);
+        float distanceToMove = camera.transform.position.x * parallaxEffect;
+
+        transform.position = new Vector3(xPosition + distanceToMove, transform.position.y);
+
+        if(distanceMoved > xPosition + spriteLength){
+            xPosition = xPosition + spriteLength;
+        }
+        else if(distanceMoved < xPosition - spriteLength){
+            xPosition = xPosition - spriteLength;
+        }
+    }
+}
